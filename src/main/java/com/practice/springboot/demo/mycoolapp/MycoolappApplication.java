d62e1183527273d4f47250bd5f5aa648a58e7c19
komal <khupsekomal@gmail.com>
2020-04-04 21:11:31 +0530

    Initial commit

---
+++ src/main/java/com/practice/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,14 @@
+package com.practice.springboot.demo.mycoolapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		run(MycoolappApplication.class, args);
+	}
+
+}
